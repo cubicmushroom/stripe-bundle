7c0148214dd4af4858ab2cc28451de11b0bfa304
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-15 12:28:52 +0100

    Started tests on StripeType form

--- spec/Form/Type/StripeTypeSpec.php
+++ spec/Form/Type/StripeTypeSpec.php
@@ -3,13 +3,58 @@
 namespace spec\CubicMushroom\Symfony\StripeBundle\Form\Type;
 
 use CubicMushroom\Symfony\StripeBundle\Form\Type\StripeType;
+use PhpSpec\Exception\Example\PendingException;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+
+/**
+ * Class StripeTypeSpec
+ *
+ * @package CubicMushroom\Symfony\StripeBundle
+ *
+ * @see     \CubicMushroom\Symfony\StripeBundle\Form\Type\StripeType
+ */
 class StripeTypeSpec extends ObjectBehavior
 {
+    /**
+     * @see StripeType
+     */
     function it_is_initializable()
     {
         $this->shouldHaveType(StripeType::class);
+    }
+
+
+    /**
+     * @see StripeType
+     */
+    function it_should_be_a_symfony_form_type()
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->shouldBeAnInstanceOf(AbstractType::class);
+    }
+
+
+    /**
+     * @uses StripeType::getName()
+     */
+    function it_should_return_name()
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->getName()->shouldReturn('cm_stripe_stripe');
+    }
+
+
+    function it_should_set_defaults(OptionsResolverInterface $resolver)
+    {
+        throw new PendingException('Update with defaults');
+        /** @noinspection PhpUndefinedMethodInspection */
+        $resolver->setDefaults(['data_class' => TakeP])->shouldBeCalled();
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->setDefaultOptions($resolver);
     }
 }
--- src/Form/Type/StripeType.php
+++ src/Form/Type/StripeType.php
@@ -2,6 +2,32 @@
 
 namespace CubicMushroom\Symfony\StripeBundle\Form\Type;
 
-class StripeType
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+/**
+ * Class StripeType
+ *
+ * @package CubicMushroom\Symfony\StripeBundle\Form\Type
+ */
+class StripeType extends AbstractType
 {
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'cm_stripe_stripe';
+    }
+
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([]);
+    }
+
+
 }
