903e505a8cf8e2d73a38770f023fbad24eac4ecd
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-15 17:42:54 +0100

    Added API keys to required bundle config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,9 +20,11 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('cm_stripe');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('api_publishable_key')->isRequired()->end()
+                ->scalarNode('api_secret_key')->isRequired()->end()
+            ->end();
 
         return $treeBuilder;
     }
