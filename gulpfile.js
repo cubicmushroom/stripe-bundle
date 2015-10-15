00a17bc8d7afae7bf754be6afae3989e3aa0ac50
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-15 12:27:53 +0100

    Added version release tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 var gulp    = require('gulp'),
-    phpspecTasks  = require('gulp-cm-phpspec-tasks');
+    phpspecTasks  = require('gulp-cm-phpspec-tasks'),
+    versionTasks = require('./gulp/version-tasks');
 
-phpspecTasks.addTasks(gulp, 'CubicMushroom\\Symfony\\StripeBundle\\');
+var namespace = 'CubicMushroom\\Symfony\\StripeBundle\\';
+
+/**
+ * PHPSpec tasks
+ */
+phpspecTasks.addTasks(gulp, namespace);
+
+/**
+ * Versioning tasks
+ */
+versionTasks.addTasks();
