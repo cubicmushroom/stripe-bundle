d964f0140376b29cc6e55a8405326d140a8af782
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-15 14:35:48 +0100

    Added code to throw an exception if data is submitted for these forms
    … as it should ever be

--- src/Form/Type/StripeInputType.php
+++ src/Form/Type/StripeInputType.php
@@ -3,6 +3,8 @@
 namespace CubicMushroom\Symfony\StripeBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
 
 /**
  * Class StripeInputType
@@ -28,6 +30,34 @@ class StripeInputType extends AbstractType
     public function getParent()
     {
         return 'text';
+    }
+
+
+    /**
+     * Just ensures that no data is submitted with this form field, as it should be protected
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addViewTransformer(
+            new CallbackTransformer(
+                function ($originalDescription) {
+                    return $originalDescription;
+                },
+                function ($submittedDescription) {
+                    if (!is_null($submittedDescription)) {
+                        throw new \RuntimeException(
+                            'Sensitive data has been submitted via you\'re form!!!  This should not happen unless '.
+                            "your server is PCI compliant.\n".
+                            'The '.__CLASS__.'::finishView() method should prevent the name attribute being added to '.
+                            'the field.'
+                        );
+                    }
+                }
+            )
+        );
     }
 
 
