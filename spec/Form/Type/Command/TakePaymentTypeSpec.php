045cfad02be445d1d0a5474a97d15399d7c0a710
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-17 20:51:16 +0100

    Started on TakePaymentType

---
+++ spec/Form/Type/Command/TakePaymentTypeSpec.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace spec\CubicMushroom\Symfony\StripeBundle\Form\Type\Command;
+
+use CubicMushroom\Payments\Stripe\Command\Payment\TakePaymentCommand;
+use CubicMushroom\Symfony\StripeBundle\Form\Type\Command\TakePaymentType;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class TakePaymentTypeSpec
+ *
+ * @package CubicMushroom\Symfony\StripeBundle
+ *
+ * @see     \CubicMushroom\Symfony\StripeBundle\Form\Type\Command\TakePaymentType
+ */
+class TakePaymentTypeSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(TakePaymentType::class);
+    }
+
+
+    function it_should_extends_symfony_abstract_class()
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->shouldBeAnInstanceOf(AbstractType::class);
+    }
+
+
+    /**
+     * @uses TakePaymentType::getName()
+     */
+    function its_name_should_be_cm_stripe_take_payment()
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->getName()->shouldReturn('cm_stripe_take_payment');
+    }
+
+
+    function it_should_use_the_take_payment_command_data_class(OptionsResolver $resolver)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->setDefaultOptions($resolver);
+
+        /** @noinspection PhpParamsInspection */ /** @noinspection PhpUndefinedMethodInspection */
+        $resolver->setDefaults(Argument::withEntry('data_class', TakePaymentCommand::class))->shouldHaveBeenCalled();
+    }
+
+    function it_should_have_fields(FormBuilderInterface $builder)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $builder->add('cost', Argument::cetera())->shouldBeCalled()->willReturn($builder);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $builder->add('stripe_form', Argument::cetera())->shouldBeCalled()->willReturn($builder);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $builder->add('description', Argument::cetera())->shouldBeCalled()->willReturn($builder);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $builder->add('userEmail', Argument::cetera())->shouldBeCalled()->willReturn($builder);
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->buildForm($builder, []);
+    }
+}
---
+++ src/Form/Type/Command/TakePaymentType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CubicMushroom\Symfony\StripeBundle\Form\Type\Command;
+
+use CubicMushroom\Payments\Stripe\Command\Payment\TakePaymentCommand;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class TakePaymentType
+ *
+ * @package CubicMushroom\Symfony\StripeBundle
+ *
+ * @see     \spec\CubicMushroom\Symfony\StripeBundle\Form\Type\Command\TakePaymentTypeSpec
+ */
+class TakePaymentType extends AbstractType
+{
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'cm_stripe_take_payment';
+    }
+
+
+    /**
+     * Sets the form defaults
+     *
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(['data_class' => TakePaymentCommand::class]);
+    }
+
+
+    /**
+     * Here for support from Symfony 3.0+
+     *
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $this->setDefaultOptions($resolver);
+    }
+
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('cost')
+            ->add('stripe_form', 'cm_form')
+            ->add('description', 'hidden')
+            ->add('userEmail', 'email');
+    }
+
+
+}
