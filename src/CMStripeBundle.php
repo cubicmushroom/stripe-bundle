81ec6b747bdae6a9a715ee54cf8a2a294eddf497
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-18 20:27:51 +0100

    Added class DocBlock

--- src/CMStripeBundle.php
+++ src/CMStripeBundle.php
@@ -4,6 +4,11 @@ namespace CubicMushroom\Symfony\StripeBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class CMStripeBundle
+ *
+ * @package CubicMushroom\Symfony\StripeBundle
+ */
 class CMStripeBundle extends Bundle
 {
 }
